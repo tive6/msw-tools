1bfe10ba2a68aa4b936404274bc4d9e66c2cdf92
zhouxianfu <zhouxianfu@yiche.com>
2022-09-15 17:04:53 +0800

    new commit: 2022/9/15 17:04:53

---
+++ tive.git.config.js
@@ -0,0 +1,22 @@
+const { argv } = process;
+const date = new Date().toLocaleString();
+const desc = (argv.length > 4 && argv.slice(-1)[0]) || `new commit: ${date}`;
+
+module.exports = {
+  shell: [
+    "git status",
+    "git add .",
+    `git commit -m "${desc}"`,
+    // 'git push origin test',
+    // 'git checkout test',
+    // 'git checkout master',
+    "git pull origin master",
+    // 'git merge --no-ff -m "clue-now merge into master" test',
+    "git push origin master",
+    // 'git checkout dev/0609-clue-now',
+    "git status",
+    // 'npm run d',
+    // 'hexo d',
+    // 'npm run push',
+  ],
+};
